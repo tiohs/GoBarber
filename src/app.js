fc22c256cf03574a39eee4643416c59024724bfc
HamiltonMassango <hamiltonsilva551@gmail.com>
2020-12-26 01:30:19 +0100

    Create model User

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from './routes';
+import './database';
 
 class App {
   constructor() {
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const router = Router();
 
-router.get('/', (req, res, next) => {
-  res.json({ ols: 'Hello Hamilton Silva 55' });
+router.get('/', async (req, res, next) => {
+  const user = await User.create({
+    name: 'Hamilton Silva',
+    email: 'hamiltonsilva551@gmail.com',
+    password_hash: '1234567890',
+  });
+  res.json(user);
 });
 
 export default router;
