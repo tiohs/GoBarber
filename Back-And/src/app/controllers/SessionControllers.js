e356c57c4a7e3848a0bb31b95964e05341683ae3
HamiltonMassango <hamiltonsilva551@gmail.com>
2021-01-09 06:53:26 +0100

    validetion session

--- Back-And/src/app/controllers/SessionControllers.js
+++ Back-And/src/app/controllers/SessionControllers.js
@@ -4,6 +4,14 @@ import authConfig from '../../config/auth';
 import {} from 'yup';
 class SessionController {
   async store(req, res) {
+    const schema = yup.object().shape({
+      email: yup.string().email().required(),
+      password: yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails ' });
+    }
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
