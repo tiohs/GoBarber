39310c59ef2746bd400b61df5e012a6ca827f249
HamiltonMassango <hamiltonsilva551@gmail.com>
2021-01-17 13:29:46 +0100

    route upload file

--- Back-And/src/routes.js
+++ Back-And/src/routes.js
@@ -1,14 +1,20 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserControllers';
 import SessionController from './app/controllers/SessionControllers';
 import authMiddleware from './app/middlewares/auth';
 
-const router = Router();
+const router = new Router();
+const upload = multer(multerConfig);
 
 router.post('/users', UserController.store);
 router.post('/sessions', SessionController.store);
 router.use(authMiddleware);
 router.put('/users', UserController.update);
+router.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default router;
