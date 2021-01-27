746cc46e29b3c1210ad032496511519678b3ff71
HamiltonMassango <hamiltonsilva551@gmail.com>
2021-01-27 14:08:23 +0100

    Add code name

--- Back-And/src/app/models/User.js
+++ Back-And/src/app/models/User.js
@@ -23,7 +23,7 @@ class User extends Model {
     return this;
   }
   static associate(models) {
-    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
   }
   checkPassword(password) {
     return compare(password, this.password_hash);
