1358e6dadd58ddec04c4b58b772a111e7c363556
HamiltonMassango <hamiltonsilva551@gmail.com>
2021-01-27 16:18:08 +0100

    validation appointment

--- Back-And/src/app/controllers/AppointmentControllers.js
+++ Back-And/src/app/controllers/AppointmentControllers.js
@@ -1,7 +1,16 @@
 import Appointment from '../models/Appointments';
+import * as Yup from 'yup';
 
 class AppointmentController {
   async store(req, res, next) {
+    const shema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+    if (!(await shema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails ' });
+    }
+
     return res.json({ ok: 'ok' });
   }
 }
