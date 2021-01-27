856b59a10079a7592133cfab8c70b6071c1ef8dc
HamiltonMassango <hamiltonsilva551@gmail.com>
2021-01-27 14:05:32 +0100

    includes File

--- Back-And/src/app/controllers/ProviderControllers.js
+++ Back-And/src/app/controllers/ProviderControllers.js
@@ -1,10 +1,12 @@
 import user from '../models/User';
+import File from '../models/File';
 
 class ProviderControllers {
   async index(req, res, net) {
     const providers = await user.findAll({
       where: { provider: false },
       attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: [File],
     });
     res.json(providers);
   }
