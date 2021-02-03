3dcdaa7cff2743f12cad6e0f4bc6f390bc87a562
HamiltonMassango <hamiltonsilva551@gmail.com>
2021-02-03 21:13:43 +0100

    add pagina and appointment

--- Back-And/src/app/controllers/AppointmentControllers.js
+++ Back-And/src/app/controllers/AppointmentControllers.js
@@ -7,11 +7,14 @@ import * as Yup from 'yup';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
     const appointment = await Appointment.findAll({
       where: {
         user_id: req.userId,
         canceled_at: null,
       },
+      limit: 20,
+      offset: (page - 1) * 20,
       order: ['date'],
       attributes: ['id', 'date'],
       include: [
