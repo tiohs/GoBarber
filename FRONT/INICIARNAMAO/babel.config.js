94cda37affce408734a8e77c1bd7ee422d3999a1
HamiltonMassango <hamiltonsilva551@gmail.com>
2021-05-06 09:27:23 +0100

    new config

--- FRONT/INICIARNAMAO/babel.config.js
+++ FRONT/INICIARNAMAO/babel.config.js
@@ -4,3 +4,9 @@ module.exports = {
     '@babel/preset-react'
   ]
 }
+
+/**
+ *  Propriedade preset 
+ * preset-env para meter as funcionadade que o navegar não entende 
+ * preset-react as propriedade do rreact que o naegador não entende 
+ */
