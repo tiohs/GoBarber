530cae0906dd8f7c6b19632a2a7d6d05bc594fa4
tiohs <hamiltonsilva551@gmail.com>
2021-06-08 20:13:48 +0100

    style component

--- FRONT/my-app/src/App.js
+++ FRONT/my-app/src/App.js
@@ -1,7 +1,12 @@
 import Routes from './routes';
-
+import GlobalStyle from "./styles/global";
 function App() {
-  return <Routes />;
+  return (
+  <>
+    <Routes />
+    <GlobalStyle />
+  </>
+  );
 }
 
 export default App;
--- FRONT/my-app/src/pages/main/index.js
+++ FRONT/my-app/src/pages/main/index.js
@@ -7,5 +7,4 @@ export default function Main () {
       <small>menor</small>
   </Title>
   );
-  
 }
