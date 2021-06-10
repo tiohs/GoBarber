4ed5bd725aee1c78855e538496f88e8868398fdc
tiohs <hamiltonsilva551@gmail.com>
2021-06-10 23:37:43 +0100

    Link para com encodeURI

--- FRONT/my-app/src/pages/main/index.js
+++ FRONT/my-app/src/pages/main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Container, Form, SubmitButton, List } from "./style";
 import { FaGithubAlt, FaPlus, FaSpinner } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 import api from '../services/api';
 
@@ -78,7 +79,7 @@ export default class Main extends Component {
             { repositories.map( repository => (
               <li key={repository.name}>
                 <span>{repository.name}</span>
-                <a href="/">Detalhes</a>
+                <Link to={`/repository/${encodeURIComponent(repository.name)}`}>Detalhes</Link>
               </li>
             ))}
         </List>
