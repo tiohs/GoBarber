7f85328afdc12d78f713eabb8ea79ef846e64ac6
tiohs <hamiltonsilva551@gmail.com>
2021-06-10 23:55:25 +0100

    repoName

--- FRONT/my-app/src/pages/Repository/index.js
+++ FRONT/my-app/src/pages/Repository/index.js
@@ -1,3 +1,15 @@
-export default function Repository ({ match }) {
-  return <h1>Repository { decodeURIComponent(match.params.repository )} </h1>
+import React, { Component } from "react";
+import api from '../services/api';
+
+export default class Repository extends Component {
+  async componentDidMount(){
+    const { match } = this.props;
+    const repoName = decodeURIComponent(match.params.repository);
+    console.log(repoName);
+  }
+  render(){
+    const { match } = this.props;
+    return <h1>Repository { decodeURIComponent(match.params.repository )} </h1>
+  }
+ 
 }
