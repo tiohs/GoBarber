fbc4db58bb7853c0708256105846f78e9df68a1e
tiohs <hamiltonsilva551@gmail.com>
2021-06-18 22:10:13 +0100

    class Acailable

---
+++ Back-And/src/app/controllers/AvailableController.js
@@ -0,0 +1,7 @@
+class AvailableController {
+  async index(req, res){
+    return ;
+  }
+}
+
+export default new AvailableController();
--- Back-And/src/routes.js
+++ Back-And/src/routes.js
@@ -11,6 +11,7 @@ import ScheduleController from './app/controllers/ScheduleControllers';
 import NotificationController from './app/controllers/NotificationControllers';
 import authMiddleware from './app/middlewares/auth';
 import NotificationController from './app/controllers/NotificationControllers';
+import AvailableController from './app/controllers/AvailableController';
 
 const router = new Router();
 const upload = multer(multerConfig);
@@ -20,6 +21,7 @@ router.post('/sessions', SessionController.store);
 router.use(authMiddleware);
 router.put('/users', UserController.update);
 router.get('/providers', ProveiderController.index);
+router.get('/providers/:providerId/available', AvailableController.index);
 router.post('/files', upload.single('file'), FileController.store);
 router.post('/appointments', AppointmentController.store);
 router.get('/appointments', AppointmentController.index);
