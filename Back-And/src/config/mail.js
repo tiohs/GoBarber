29a2096d1b5b1d9b7e69f4254272216f6f8ff6c8
tiohs <hamiltonsilva551@gmail.com>
2021-06-19 14:18:11 +0100

    ok

--- Back-And/src/config/mail.js
+++ Back-And/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: "smtp.mailtrap.io",
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: "73c5b7024895fb",
-    pass: "4f3ae0bd91cc91"
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   },
   default: {
     from: 'Equipe GoBarber <hamilton@gobarber.com>',
