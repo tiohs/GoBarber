b755e7b718352ba1ea3efd7acd26ca4b2d82ceb5
tiohs <hamiltonsilva551@gmail.com>
2021-06-29 20:47:57 +0100

    thead

--- FRONT/rocktenis/src/pages/Cart/styles.js
+++ FRONT/rocktenis/src/pages/Cart/styles.js
@@ -1,9 +1,20 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-
+  padding: 30px;
+  background: #fff;
+  border-radius: 4px;
 `;
 
-export const  ProductTable = styled.table``;
+export const  ProductTable = styled.table`
+  width: 100%;
+  thead th {
+    color: #999;
+    text-align: left;
+    padding: 12px;
+  }
+
+  
+`;
 
 export const  Total = styled.div``;
