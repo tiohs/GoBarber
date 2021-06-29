41f7b625007e64e812f7a84dba2f9026a950de69
tiohs <hamiltonsilva551@gmail.com>
2021-06-29 21:54:05 +0100

    init config api

--- FRONT/rocktenis/src/pages/Cart/styles.js
+++ FRONT/rocktenis/src/pages/Cart/styles.js
@@ -1,9 +1,31 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.div`
   padding: 30px;
   background: #fff;
   border-radius: 4px;
+
+  footer {
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+    button {
+      background: #7159c1;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      padding: 12px 20px;
+      font-weight: bold;
+      text-transform: uppercase;
+      transition: background 0.2s;
+      &:hover {
+        background: ${darken(0.03, '#7159c1')};
+      }
+    }
+  }
 `;
 
 export const  ProductTable = styled.table`
@@ -50,4 +72,17 @@ export const  ProductTable = styled.table`
   }
 `;
 
-export const  Total = styled.div``;
+export const  Total = styled.div`
+  display: flex;
+  align-items: baseline;
+
+  span {
+    color: #999;
+    font-weight: bold;
+  }
+
+  strong {
+    font-size: 20px;
+    margin-left: 5px;
+  }
+`;
