16e143dbd0bfdff7591dcb6642017fc4cf9ff1ed
tiohs <hamiltonsilva551@gmail.com>
2021-06-29 18:36:49 +0100

    finish header

--- FRONT/rocktenis/src/components/Header/style.js
+++ FRONT/rocktenis/src/components/Header/style.js
@@ -12,6 +12,10 @@ export const Cart = styled(Link)`
   display: flex;
   align-items: center;
   text-decoration: none;
+  transition: opacity 0.2s;
+  &:hover {
+    opacity: 0.7;
+  }
   div {
     text-align: right;
     margin-right: 10px;
@@ -24,7 +28,5 @@ export const Cart = styled(Link)`
         font-size: 12px;
         color: #999;
       }
-  
-   
   }
 `;
