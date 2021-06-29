4855bfa13927dddc4129d9f6e00c9fe302a0e816
tiohs <hamiltonsilva551@gmail.com>
2021-06-29 23:02:59 +0100

    transformando function em class

--- FRONT/rocktenis/src/pages/Home/index.js
+++ FRONT/rocktenis/src/pages/Home/index.js
@@ -1,55 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ProductList } from './styles';
 import { MdAddShoppingCart } from 'react-icons/md';
 import tenis from '../../assets/images/ok.jpg';
+import api from '../../services/api';
 
-export default function Home() { 
-  return  (<ProductList>
-    <li>
-      <img src={tenis} alt="" />
-      <strong>Tênis muito legal</strong>
-      <span>R$129,90</span>
-      <button type="button">
-        <div>
-          <MdAddShoppingCart size = {16 } color="#FFF"/> 3
-        </div>
-        <span> ADICIONAR AO CARRINHO </span>
-      </button>
-    </li>
-    <li>
-      <img src={tenis} alt="" />
-      <strong>Tênis muito legal</strong>
-      <span>R$129,90</span>
-      <button type="button">
-        <div>
-          <MdAddShoppingCart size = {16 } color="#FFF"/> 3
-        </div>
-        <span> ADICIONAR AO CARRINHO </span>
-      </button>
-    </li>
-    <li>
-      <img src={tenis} alt="" />
-      <strong>Tênis muito legal</strong>
-      <span>R$129,90</span>
-      <button type="button">
-        <div>
-          <MdAddShoppingCart size = {16 } color="#FFF"/> 3
-        </div>
-        <span> ADICIONAR AO CARRINHO </span>
-      </button>
-    </li>
-    <li>
-      <img src={tenis} alt="" />
-      <strong>Tênis muito legal</strong>
-      <span>R$129,90</span>
-      <button type="button">
-        <div>
-          <MdAddShoppingCart size = {16 } color="#FFF"/> 3
-        </div>
-        <span> ADICIONAR AO CARRINHO </span>
-      </button>
-    </li>
-    
-  </ProductList>)
+export default class Home extends Component { 
+  render (){
+    return  (<ProductList>
+      <li>
+        <img src={tenis} alt="" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size = {16 } color="#FFF"/> 3
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img src={tenis} alt="" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size = {16 } color="#FFF"/> 3
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img src={tenis} alt="" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size = {16 } color="#FFF"/> 3
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img src={tenis} alt="" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size = {16 } color="#FFF"/> 3
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      
+    </ProductList>)
+  
+  }
+ 
 }
 
