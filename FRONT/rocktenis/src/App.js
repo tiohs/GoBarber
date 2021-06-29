0dfe9edc38b573ed3a9fcf1006538b73d19c3ea1
tiohs <hamiltonsilva551@gmail.com>
2021-06-29 18:04:22 +0100

    import logo

--- FRONT/rocktenis/src/App.js
+++ FRONT/rocktenis/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
+import Header from './components/Header';
 import Routes from './routes';
 
 import GlobalStyle from './style/globals';
@@ -7,7 +9,7 @@ function App() {
 	return (
 		<BrowserRouter>
 		<GlobalStyle />
-			{/* <Header /> */}
+	 	<Header /> 
 			<Routes />
 		</BrowserRouter>
 	);
