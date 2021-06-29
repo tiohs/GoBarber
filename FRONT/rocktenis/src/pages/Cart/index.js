1ea09636cc4ff89974051a03b546c58283a954a0
tiohs <hamiltonsilva551@gmail.com>
2021-06-29 20:42:58 +0100

    finish sintaxe

--- FRONT/rocktenis/src/pages/Cart/index.js
+++ FRONT/rocktenis/src/pages/Cart/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
 import {Container, ProductTable, Total } from './styles';
+import tenis from '../../assets/images/ok.jpg';
 
 export default function Cart() { 
   return (<Container>
@@ -16,7 +18,34 @@ export default function Cart() {
       </thead>
       <tbody>
         <tr>
-          
+          <td>
+            <img src={tenis} alt="Tenis" />
+          </td>
+          <td>
+            <strong>
+              Tênis muito massa 
+            </strong>
+            <span>
+              R$129,90
+            </span>
+          </td>
+          <td>
+            <button>
+              <MdRemoveCircleOutline size={20} color="#7159c1"/>
+            </button>
+            <input type="numeber" readOnly value={2}/>
+            <button>
+              <MdAddCircleOutline size={20} color="#7159c1"/>
+            </button>
+          </td>
+          <td>
+            <strong>R$ 258,80</strong>
+          </td>
+          <td>
+            <button type="button">
+              <MdDelete size={20} color="#7159c1"/>
+            </button>
+          </td>
         </tr>
       </tbody>
     </ProductTable>
