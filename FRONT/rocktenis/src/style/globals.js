341cf9800712271cecfeba2d9bfe83b9f898e4bb
tiohs <hamiltonsilva551@gmail.com>
2021-06-29 15:31:50 +0100

    global style

---
+++ FRONT/rocktenis/src/style/globals.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background: #191919;
+    --webkit-font-smoothing: antialiased;
+  }
+  body, input, button {
+    font: 14px sans-serif;
+  } 
+`;
