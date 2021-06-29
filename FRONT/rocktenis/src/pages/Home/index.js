b62262370de612d2048bc5419bbefd2512f7ae9f
tiohs <hamiltonsilva551@gmail.com>
2021-06-29 18:52:36 +0100

    add images exemplo

--- FRONT/rocktenis/src/pages/Home/index.js
+++ FRONT/rocktenis/src/pages/Home/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { ProductList } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import tenis from '../../assets/images/ok.jpg';
 
 export default function Home() { 
-  return  (<h1>Home</h1>);
+  return  <ProductList>
+    <li>
+      <img src={tenis} alt="" />
+      <strong>Tênis muito legal</strong>
+      <span>R$129,90</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size = {16 } color="#FFF"/> 3
+        </div>
+        <span> ADICIONAR AO CARRINHO </span>
+      </button>
+    </li>
+  </ProductList>
 }
 
