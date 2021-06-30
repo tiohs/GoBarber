f54938b7f77f1e9d1a488ad9cf977c995b444d87
tiohs <hamiltonsilva551@gmail.com>
2021-06-30 11:25:51 +0100

    transforme em class

--- FRONT/rocktenis/src/pages/Cart/index.js
+++ FRONT/rocktenis/src/pages/Cart/index.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
 import {Container, ProductTable, Total } from './styles';
 import tenis from '../../assets/images/ok.jpg';
 
-export default function Cart() { 
-  return (<Container>
-    <ProductTable>
-      <thead>
-        <tr>
-          <th />
-          <th>PRODUTO</th>
-          <th>QTD</th>
-          <th>SUBTOTAL</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
+class Cart extends Component { 
+  
+  render(){
+    const { cart } = this.props;
+    return (<Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          { cart.map(product => (
+
+          <tr>
           <td>
             <img src={tenis} alt="Tenis" />
           </td>
@@ -49,15 +55,21 @@ export default function Cart() {
             </button>
           </td>
         </tr>
-      </tbody>
-    </ProductTable>
-    <footer>
-      <button type="button">Finalizar pedido </button>
-      <Total>
-        <span>Total</span>
-        <strong>R$1920,28</strong>
-      </Total>
-    </footer>
-  </Container>);
+          ))
+        }</tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido </button>
+        <Total>
+          <span>Total</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>);
+  }
+  
 }
-
+const mapStateToProps = state => ({
+  cart : state.cart,
+});
+export default connect(mapStateToProps)(Cart);
