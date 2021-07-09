5f133739ac75839e3d71a4a6cea09672eb86e830
tiohs <hamiltonsilva551@gmail.com>
2021-07-09 22:20:54 +0100

    sagas finish

--- FRONT/rocktenis/src/store/modules/cart/sagas.js
+++ FRONT/rocktenis/src/store/modules/cart/sagas.js
@@ -1,6 +1,10 @@
-import { call } from 'redux-saga/effects';
+import { call, put, all, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
+import { addToCartSucess } from './actions';
 
 function* addToCart(id) {
   const response = yield call(api.get, `/products/${id}`);
+  yield put(addToCartSucess(response.date));
 }
+
+export default all([takeLatest('@Cart/ADD_REQUEST', addToCart)]);
