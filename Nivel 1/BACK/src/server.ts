0becf3814f09c90be5650b5f8673fb987ff6a818
tiohs <hamiltonsilva551@gmail.com>
2021-11-10 14:14:05 +0100

    config past

--- Nivel 1/BACK/src/server.ts
+++ Nivel 1/BACK/src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+  response.json({ ok : 'Tudo bem '});
+});
 app.listen(3333, () => {
   console.log('Localhos port 3333')
 });
