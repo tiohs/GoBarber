d8f880a05ba99a687ef39376bc87e0aaa369ed25
tiohs <hamiltonsilva551@gmail.com>
2021-11-10 15:07:04 +0100

    import default

--- Nivel 1/BACK/src/routes/index.ts
+++ Nivel 1/BACK/src/routes/index.ts
@@ -2,4 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
+routes.get('/', (request, response) => {
+  response.json({ ok: 'Tudo bem ' });
+});
+
 export default routes;
--- Nivel 1/BACK/src/server.ts
+++ Nivel 1/BACK/src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({ ok: 'Tudo bem ' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Localhos port 3333');
