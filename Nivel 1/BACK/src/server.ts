841ed8f5275d2a6f1db25e501893e7f958f10d39
tiohs <hamiltonsilva551@gmail.com>
2021-11-10 14:54:33 +0100

    cool config eslint

--- Nivel 1/BACK/src/server.ts
+++ Nivel 1/BACK/src/server.ts
@@ -3,9 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  response.json({ ok : 'Tudo bem '});
+  response.json({ ok: 'Tudo bem ' });
 });
 
 app.listen(3333, () => {
-  console.log('Localhos port 3333')
+  console.log('Localhos port 3333');
 });
