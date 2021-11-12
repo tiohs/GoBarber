32298cc747d2cff6a30cae45a7d406d029568685
tiohs <hamiltonsilva551@gmail.com>
2021-11-12 17:18:33 +0100

    There is a error

--- Nivel 2/BACK/src/routes/appointments.routes.ts
+++ Nivel 2/BACK/src/routes/appointments.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
-import { parseISO } from 'date-fns';
+// import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
-appointmentsRouter.get('/', (request, response) => {
+appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = appointmentsRepository.find();
+  const appointments = await appointmentsRepository.find();
   return response.json(appointments);
 });
 
@@ -17,8 +17,8 @@ appointmentsRouter.post('/', (request, response) => {
   try {
     const { provider, date } = request.body;
 
-    const parsedDate = parseISO(date);
-
+    // const parsedDate = parseISO(date);
+    const parsedDate = date;
     const createAppointment = new CreateAppointmentService();
 
     const appointment = createAppointment.execute({
@@ -28,7 +28,7 @@ appointmentsRouter.post('/', (request, response) => {
 
     return response.json(appointment);
   } catch (error) {
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error });
   }
 });
 
