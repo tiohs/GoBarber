bff4bd39597739d2fc89ef4cc1b20a65078baf26
tiohs <hamiltonsilva551@gmail.com>
2021-11-13 12:28:05 +0100

    add Came Case TypeScript eslint

--- Nivel 2/BACK/src/routes/index.ts
+++ Nivel 2/BACK/src/routes/index.ts
@@ -5,7 +5,7 @@ import usersRouter from './users.routes';
 
 const routes = Router();
 
-routes.use(appointmentsRouter);
-routes.use(usersRouter);
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
--- Nivel 2/BACK/src/routes/users.routes.ts
+++ Nivel 2/BACK/src/routes/users.routes.ts
@@ -1,12 +1,22 @@
 import { Router } from 'express';
 
+import CreateUserSevice from '../services/CreateUserSevice';
+
 const usersRouter = Router();
 
 usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
 
-    return response.json({});
+    const createUser = new CreateUserSevice();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
   } catch ({ message }) {
     return response.status(400).json({ error: message });
   }
