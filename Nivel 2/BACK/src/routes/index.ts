85338646292c053ab6372ae3f57c1ba211882ceb
tiohs <hamiltonsilva551@gmail.com>
2021-11-13 12:09:24 +0100

    CreateUserService

--- Nivel 2/BACK/src/routes/index.ts
+++ Nivel 2/BACK/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use(appointmentsRouter);
+routes.use(usersRouter);
 
 export default routes;
