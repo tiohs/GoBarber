49b69d1f4a66798d4348cd160a1efde7f2b56364
tiohs <hamiltonsilva551@gmail.com>
2021-11-20 20:39:26 +0100

    uploads static

--- Nivel 2/BACK/src/server.ts
+++ Nivel 2/BACK/src/server.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata';
-
 import express from 'express';
 
+import configAvatar from './config/uploads';
 import routes from './routes';
 import './database';
 
 const app = express();
+
 app.use(express.json());
+app.use('/files', express.static(configAvatar.directory));
 app.use(routes);
 
 app.listen(4000, () => {
