a225d749127992d7f8121f53d84a4eee977bfa26
tiohs <hamiltonsilva551@gmail.com>
2022-01-05 15:35:00 +0100

    cool

--- Nivel 2/Front-End/project-react-typescript/src/pages/Dashboard/index.tsx
+++ Nivel 2/Front-End/project-react-typescript/src/pages/Dashboard/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
-import { Title } from './style';
+import { Title, Form, Repositories } from './style';
+import avatar from "../../assets/img.jpg";
 
 const Dashboard: React.FC = () => {
   return (
-    <Title>Explore repositórios no GitHub</Title>
+    <>
+      <Title>Explore repositórios no GitHub</Title>
+      <Form>
+        <input type="text" placeholder='Deigite o nome do repositório '/>
+        <button type='submit'>Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="test">
+          <img src={avatar} alt="Hamilton Silva"  />
+          <div>
+            <strong>unituenda/pilha</strong>
+            <p>Melhor software de procura de ajuda !</p>
+          </div>
+
+        </a>
+      </Repositories>
+    </>
   );
 }
 
--- Nivel 2/Front-End/project-react-typescript/src/pages/Dashboard/style.ts
+++ Nivel 2/Front-End/project-react-typescript/src/pages/Dashboard/style.ts
@@ -1,7 +1,72 @@
 import styled from "styled-components";
 
 export const Title = styled.h1`
-  font-size: 48px;
+  font-size: 40px;
   color: #3a3a3a;
 `;
 
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+
+  display: flex;
+  input {
+    flex: 1;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04d361;
+    border-radius: 0px 5px 5px 0px;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+   
+    &:hover {
+      background: #04d361;
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+  }
+
+  img {
+    width: 64px;
+    height: 64px;
+    border-radius: 50%;
+  }
+  div {
+    margin-left: 16px;
+    strong {
+      font-size: 20px;
+      color: #3d3d3d;
+    }
+    p {
+      font-size: 18px;
+      color: #a8a8a3;
+      margin-top: 4px;
+    }
+  }
+`;
