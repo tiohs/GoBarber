5399725380706a13099099c109289993037cdb5e
tiohs <hamiltonsilva551@gmail.com>
2022-02-03 16:53:56 +0100

    redirect link

--- Nivel 2/Front-End/project-react-typescript/src/pages/Dashboard/index.tsx
+++ Nivel 2/Front-End/project-react-typescript/src/pages/Dashboard/index.tsx
@@ -59,7 +59,7 @@ const Dashboard: React.FC = () => {
        { repositories.map(repository => (
         <Link 
           key={repository.full_name} 
-          to={`${repository.full_name}`}
+          to={`repository/${repository.full_name}`}
         >
           <img 
             src={repository.owner.avatar_url} 
