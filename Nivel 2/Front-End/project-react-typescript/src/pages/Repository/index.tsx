70dc7475b3b709e7e748ccd54449ed134d44bf2d
tiohs <hamiltonsilva551@gmail.com>
2022-02-05 15:57:42 +0100

    header

--- Nivel 2/Front-End/project-react-typescript/src/pages/Repository/index.tsx
+++ Nivel 2/Front-End/project-react-typescript/src/pages/Repository/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { FiChevronsLeft } from 'react-icons/fi';
+import logoImg from '../../assets/attachment.svg'
 import api from '../../services/api';
+import { Header } from './style';
 
 const Repository: React.FC = () => {
   let { id } = useParams();
   id = id?.replace('_', '/');
 
   return (
-    <h1>{id}</h1>
-
+    <Header>
+      <img src={logoImg} alt="Github Explorer" />
+      <Link to="/dashboard">
+      <FiChevronsLeft size={32} />
+        <p>Volta</p>
+      </Link>     
+    </Header>
   );
 }
 
