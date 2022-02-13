3b5ec28f0b31bdf30780df525578ae5fb780628a
tiohs <hamiltonsilva551@gmail.com>
2022-02-13 23:58:31 +0100

    update

--- Nivel 3/Gobarber/front/src/App.tsx
+++ Nivel 3/Gobarber/front/src/App.tsx
@@ -1,11 +1,4 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-     Hello World !
-    </div>
-  );
-}
-
+const App: React.FC = () => <div>Hello World !</div>;
 export default App;
