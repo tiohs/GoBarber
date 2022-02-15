75434ef677d9407cab3ae71e5cc7f8719cdf8c8f
tiohs <hamiltonsilva551@gmail.com>
2022-02-15 11:53:16 +0100

    styled

--- Nivel 3/Gobarber/front/src/pages/seignin/index.tsx
+++ Nivel 3/Gobarber/front/src/pages/seignin/index.tsx
@@ -1,6 +1,8 @@
 import { Sign } from 'crypto';
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
+
 import { Container, Content, Background } from './style';
 
 const SignIn: React.FC = () => (
@@ -22,7 +24,12 @@ const SignIn: React.FC = () => (
           placeholder="Digite a password"
         />
         <button type="submit">Enviar</button>
+        <a href="forgot">Esqueci minha senha</a>
       </form>
+      <a href="login">
+        <FiLogIn />
+        Criar conta
+      </a>
     </Content>
     <Background />
   </Container>
