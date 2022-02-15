51b6457603c01d7cb12ba28d05be6ce5afae4d9e
tiohs <hamiltonsilva551@gmail.com>
2022-02-15 11:38:19 +0100

    upload img logo

--- Nivel 3/Gobarber/front/src/pages/seignin/index.tsx
+++ Nivel 3/Gobarber/front/src/pages/seignin/index.tsx
@@ -6,6 +6,7 @@ import { Container, Content, Background } from './style';
 const SignIn: React.FC = () => (
   <Container>
     <Content>
+      <img src={logoImg} alt="Logo da goBarber" />
       <form action="post">
         <h1>Faça seu logon</h1>
         <input
