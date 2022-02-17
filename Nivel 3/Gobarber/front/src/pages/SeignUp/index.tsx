33690847e08793ba332ba3c6bc815f996403d46c
tiohs <hamiltonsilva551@gmail.com>
2022-02-17 08:54:55 +0100

    ok

---
+++ Nivel 3/Gobarber/front/src/pages/SeignUp/index.tsx
@@ -0,0 +1,49 @@
+import { Sign } from 'crypto';
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './style';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="Logo da goBarber" />
+      <form action="post">
+        <h1>Faça seu cadastro</h1>
+        <Input
+          icon={FiUser}
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Digite o seu nome "
+        />
+        <Input
+          icon={FiMail}
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Digite o seu email "
+        />
+        <Input
+          icon={FiLock}
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Digite a password"
+        />
+        <Button>Enviar</Button>
+      </form>
+      <a href="login">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+
+export default SignUp;
