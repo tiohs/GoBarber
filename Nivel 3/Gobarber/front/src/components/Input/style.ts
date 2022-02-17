7a926e7ed9a2e261eac0fbf05dbcf39636098c60
tiohs <hamiltonsilva551@gmail.com>
2022-02-17 08:25:10 +0100

    update style components

--- Nivel 3/Gobarber/front/src/components/Input/style.ts
+++ Nivel 3/Gobarber/front/src/components/Input/style.ts
@@ -1,18 +1,27 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  color: #666368;
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  padding: 16px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  & + div {
+    margin-top: 8px;
+  }
   input {
     color: #f4ede8;
-    background: #232129;
-    border-radius: 10px;
-    border: 2px solid #232129;
-    padding: 16px;
-    width: 100%;
-    & + input {
-      margin-top: 8px;
-    }
+    flex: 1;
+    background: transparent;
+    border: 0;
     &::placeholder {
       color: #666368;
     }
+  }
+  svg {
+    margin-right: 12px;
   }
 `;
