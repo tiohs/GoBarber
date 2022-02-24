3bf3073d1b97900ad192321376685cc863b3c006
tiohs <hamiltonsilva551@gmail.com>
2022-02-24 20:53:45 +0100

    error

--- Nivel 3/Gobarber/front/src/pages/SeignUp/index.tsx
+++ Nivel 3/Gobarber/front/src/pages/SeignUp/index.tsx
@@ -2,12 +2,14 @@ import React, { useCallback, useRef } from 'react';
 import { Form } from '@unform/web';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import * as Yup from 'yup';
+import { ValidationError } from 'yup';
+
 import { FormHandles } from '@unform/core';
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-
+import getValidationErrors from '../../utils/getValidationErros';
 import { Container, Content, Background } from './style';
 
 const SignUp: React.FC = () => {
@@ -15,8 +17,9 @@ const SignUp: React.FC = () => {
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
       const schema = Yup.object().shape({
-        name: Yup.object().required('Nome obrigatório'),
+        name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
           .required('E-mail obrigatório')
           .email('Digite um email válido'),
@@ -26,9 +29,8 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     } catch (error) {
-      formRef.current?.setErrors({
-        name: 'Nome obrigatório',
-      });
+      const err = error as ValidationError;
+      formRef.current?.setErrors(getValidationErrors(err));
       console.log(error);
     }
   }, []);
