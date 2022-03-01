1dc905fe9c53bc8e97a594301d99de1296389fdd
tiohs <hamiltonsilva551@gmail.com>
2022-03-01 14:26:09 +0100

    hook useAuth

--- Nivel 3/Gobarber/front/src/hooks/AuthContext.tsx
+++ Nivel 3/Gobarber/front/src/hooks/AuthContext.tsx
@@ -9,6 +9,7 @@ interface SignInCredencials {
 interface AuthContextData {
   user: object;
   signIn(credencials: SignInCredencials): Promise<void>;
+  signOut(): void;
 }
 interface AuthState {
   token: string;
@@ -39,8 +40,14 @@ export const AuthProvider: React.FC = ({ children }) => {
     setData({ token, user });
   }, []);
 
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+    setData({} as AuthState);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
