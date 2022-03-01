51a1ef5433b92157bb4196a29cbefe232d532da5
tiohs <hamiltonsilva551@gmail.com>
2022-03-01 11:47:02 +0100

    hooks contextAuth

--- Nivel 3/BACK/src/server.ts
+++ Nivel 3/BACK/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import configAvatar from './config/uploads';
 import routes from './routes';
@@ -9,6 +10,7 @@ import AppError from './error/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(configAvatar.directory));
 app.use(routes);
