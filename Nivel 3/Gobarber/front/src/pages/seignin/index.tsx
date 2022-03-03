a2ffa551c67e97ac0f8c3a9e61bd4aa64cca3957
tiohs <hamiltonsilva551@gmail.com>
2022-03-03 15:57:14 +0100

    toast add Success

--- Nivel 3/Gobarber/front/src/pages/seignin/index.tsx
+++ Nivel 3/Gobarber/front/src/pages/seignin/index.tsx
@@ -7,6 +7,8 @@ import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
 import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -22,7 +24,7 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const { signIn } = useAuth();
-
+  const { addToast } = useToast();
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
       try {
@@ -36,7 +38,7 @@ const SignIn: React.FC = () => {
         await schema.validate(data, {
           abortEarly: false,
         });
-        signIn({
+        await signIn({
           email: data.email,
           password: data.password,
         });
@@ -45,10 +47,10 @@ const SignIn: React.FC = () => {
           const err = error as ValidationError;
           formRef.current?.setErrors(getValidationErrors(err));
         }
-        // disparar um toast
+        addToast();
       }
     },
-    [signIn],
+    [signIn, addToast],
   );
   return (
     <Container>
