997a71102e1ad344f20b39bf9f87bce15bea8d68
tiohs <hamiltonsilva551@gmail.com>
2022-03-08 10:37:17 +0100

    animation login

--- Nivel 3/Gobarber/front/src/pages/seignin/style.ts
+++ Nivel 3/Gobarber/front/src/pages/seignin/style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 import signInBackground from '../../assets/sign-in-background.png';
 
@@ -21,10 +21,21 @@ export const Content = styled.div`
   max-width: 700px;
 `;
 
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
 export const AnimationContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  animation: ${appearFromLeft} 1s;
   form {
     margin: 80px 0;
     width: 340px;
