c8dbd12186863c40a512b5049881ea4e3364bc33
tiohs <hamiltonsilva551@gmail.com>
2022-03-09 21:56:54 +0100

    router authentication

--- Nivel 3/Gobarber/front/src/routes/RoutePublic.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-const routes: React.FC = () => {
-  return <div />;
-};
-
-export default routes;
--- Nivel 3/Gobarber/front/src/routes/index.tsx
+++ Nivel 3/Gobarber/front/src/routes/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import SignIn from '../pages/seignin/index';
 import SignUp from '../pages/SeignUp/index';
 import Dashboard from '../pages/Dashboard';
 
+import Route from './Route';
+
 const Routers: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/SignUp" component={SignUp} />
-    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
