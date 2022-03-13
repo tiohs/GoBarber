c1c16097172f4a09d672c6165f467d315c09f474
tiohs <hamiltonsilva551@gmail.com>
2022-03-13 13:15:23 +0100

    routes appointement

--- Nivel 3/BACK/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ Nivel 3/BACK/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,9 +2,9 @@ import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
-import CreateAppointmentService from '../services/CreateAppointmentService';
-import ensureAuthenticated from '../middleware/ensureAuthenticated';
+import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import ensureAuthenticated from '@shared/infra/http/middleware/ensureAuthenticated';
 
 const appointmentsRouter = Router();
 appointmentsRouter.use(ensureAuthenticated);
