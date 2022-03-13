b7deb616ce3a8d977bd5e7f714f0e7d314c7b67c
tiohs <hamiltonsilva551@gmail.com>
2022-03-13 13:27:16 +0100

    ok

--- Nivel 3/BACK/src/modules/appointments/infra/typeorm/entities/Appointment.ts
+++ Nivel 3/BACK/src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -2,7 +2,7 @@ import {
   Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn,
 } from 'typeorm';
 
-import User from './Users';
+import User from '@modules/users/infra/typeorm/entities/Users';
 
 @Entity('appointments')
 class Appointment {
