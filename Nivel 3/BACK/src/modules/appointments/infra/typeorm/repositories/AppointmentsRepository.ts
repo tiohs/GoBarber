1e662e995e494bb4bf27e0077750a8511aadcea6
tiohs <hamiltonsilva551@gmail.com>
2022-03-17 18:24:13 +0100

    SOLID use L

--- Nivel 3/BACK/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ Nivel 3/BACK/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,13 +1,14 @@
 import { EntityRepository, Repository } from 'typeorm';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
-  public async findByDate(date: Date): Promise<Appointment | null> {
+class AppointmentsRepository extends Repository<Appointment> implements IAppointmentsRepository {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     const findAppointment = await this.findOne({
       where: { date },
     });
-    return findAppointment || null;
+    return findAppointment;
   }
 }
 
--- Nivel 3/BACK/src/modules/appointments/services/CreateAppointmentService.ts
+++ Nivel 3/BACK/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,7 +1,7 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import AppError from '@shared/error/AppError';
 
 interface Request {
