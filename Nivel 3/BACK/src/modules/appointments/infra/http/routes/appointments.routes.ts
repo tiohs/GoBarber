c97db7b74208fe558ca9966415c18a2795d685ca
tiohs <hamiltonsilva551@gmail.com>
2022-07-23 23:55:20 +0100

    appointements

--- Nivel 3/BACK/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ Nivel 3/BACK/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
 
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
+import { container } from 'tsyringe';
 
 const appointmentsRouter = Router();
 
@@ -16,10 +16,9 @@ appointmentsRouter.use(ensureAuthenticated);
 // });
 
 appointmentsRouter.post('/', async (request, response) => {
-  const appointmentsRepository = new AppointmentsRepository();
   const { provider_id, date } = request.body;
   const parsedDate = parseISO(date);
-  const createAppointment = new CreateAppointmentService(appointmentsRepository);
+  const createAppointment = container.resolve(CreateAppointmentService);
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
