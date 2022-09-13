4222310550c4e257567e8d771dfd3ee4232795cd
tiohs <hamiltonsilva551@gmail.com>
2022-09-13 12:33:53 +0100

    Error

---
+++ Nivel 3/BACK/src/modules/appointments/repositories/fakes/FakesAppointmentsRepository.ts
@@ -0,0 +1,25 @@
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+class FakeAppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find((appointment) => appointment.date === date);
+
+    return findAppointment;
+  }
+
+  public async create({ provider_id, date }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: Math.random().toString().slice(2), date, provider_id });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default FakeAppointmentsRepository;
--- Nivel 3/BACK/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ Nivel 3/BACK/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,19 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakesAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(fakeAppointmentsRepository);
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '1',
+    });
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1');
+  });
+
+  // it('should not be able to create two appointment on the same time', () => {
+  //   expect(1 + 3).toBe(4);
+  // });
 });
