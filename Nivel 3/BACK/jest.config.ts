0c3861d9937c63a18b3b0d1d2a4c5d8994cbda99
tiohs <hamiltonsilva551@gmail.com>
2022-09-13 12:33:48 +0100

    Error

--- Nivel 3/BACK/jest.config.ts
+++ Nivel 3/BACK/jest.config.ts
@@ -1,8 +1,3 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
-
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -88,7 +83,6 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
