35b747881d50d4d5ab9699a89b403ccb0c3c56d0
tiohs <hamiltonsilva551@gmail.com>
2022-09-27 12:31:31 +0100

    Should not be able to recover anythings email

--- Nivel 3/BACK/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ Nivel 3/BACK/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,10 +1,10 @@
-// import AppError from '@shared/error/AppError';
+import AppError from '@shared/error/AppError';
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeIMailProvider';
 import FakeUsersRepository from '../Repositories/fakes/FakeUsersRepository';
 import SendForgetPasswordEmailService from './SendForgotPasswordEmailService';
 
-describe('CreateUsers', () => {
-  it('should be able to recover the password using the email', async () => {
+describe('SendForgetPasswordEmail', () => {
+  it('Should be able to recover the password using the email', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const fakeMailProvider = new FakeMailProvider();
 
@@ -23,5 +23,17 @@ describe('CreateUsers', () => {
       email: 'johondoe@exemple.com',
     });
     expect(sendMail).toHaveBeenCalled();
+  });
+
+  it('Should not be able to recover a non-existing user password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    // eslint-disable-next-line max-len
+    const sendForgetPasswordEmailService = new SendForgetPasswordEmailService(fakeUsersRepository, fakeMailProvider);
+
+    expect(sendForgetPasswordEmailService.execute({
+      email: 'johondoe@exemple.com',
+    })).rejects.toBeInstanceOf(AppError);
   });
 });
