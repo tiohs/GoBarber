6a2fc69f3989c7c69c8195e32a007ea3cf532a76
tiohs <hamiltonsilva551@gmail.com>
2022-09-27 14:40:22 +0100

    Cool

--- Nivel 3/BACK/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ Nivel 3/BACK/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -2,15 +2,22 @@ import AppError from '@shared/error/AppError';
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeIMailProvider';
 import FakeUsersRepository from '../Repositories/fakes/FakeUsersRepository';
 import SendForgetPasswordEmailService from './SendForgotPasswordEmailService';
+import FakeUserTokenRepository from '../Repositories/fakes/FakeUsersTokenRepository';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeUserTokenRepository: FakeUserTokenRepository;
+let fakeMailProvider: FakeMailProvider;
+let sendForgetPasswordEmailService: SendForgetPasswordEmailService;
 
 describe('SendForgetPasswordEmail', () => {
-  it('Should be able to recover the password using the email', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeMailProvider = new FakeMailProvider();
-
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeMailProvider = new FakeMailProvider();
+    fakeUserTokenRepository = new FakeUserTokenRepository();
     // eslint-disable-next-line max-len
-    const sendForgetPasswordEmailService = new SendForgetPasswordEmailService(fakeUsersRepository, fakeMailProvider);
-
+    sendForgetPasswordEmailService = new SendForgetPasswordEmailService(fakeUsersRepository, fakeMailProvider, fakeUserTokenRepository);
+  });
+  it('Should be able to recover the password using the email', async () => {
     const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
 
     await fakeUsersRepository.create({
@@ -26,14 +33,20 @@ describe('SendForgetPasswordEmail', () => {
   });
 
   it('Should not be able to recover a non-existing user password', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeMailProvider = new FakeMailProvider();
-
-    // eslint-disable-next-line max-len
-    const sendForgetPasswordEmailService = new SendForgetPasswordEmailService(fakeUsersRepository, fakeMailProvider);
-
     await expect(sendForgetPasswordEmailService.execute({
       email: 'johondoe@exemple.com',
     })).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should be able to generate key for user', async () => {
+    const UserToken = jest.spyOn(fakeUserTokenRepository, 'generate');
+
+    const user = await fakeUsersRepository.create({
+      email: 'johondoe@exemple.com',
+      name: 'John Doe',
+      password: '123456',
+    });
+    await sendForgetPasswordEmailService.execute({ email: 'johondoe@exemple.com' });
+    await expect(UserToken).toBeCalledWith(user.id);
   });
 });
--- Nivel 3/BACK/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ Nivel 3/BACK/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import AppError from '@shared/error/AppError';
 import IUsersRepository from '../Repositories/IUsersRepository';
+import IUserTokensRepository from '../Repositories/IUserTokensRepository';
 
 interface IRequest {
   email: string,
@@ -14,14 +15,18 @@ class CreateUserService {
     private usersRepository: IUsersRepository,
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
-    const checkUserExists = await this.usersRepository.findByEmail(email);
+    const user = await this.usersRepository.findByEmail(email);
 
-    if (!checkUserExists) {
+    if (!user) {
       throw new AppError('Users does not exist ');
     }
+
+    this.userTokensRepository.generate(user.id);
     this.mailProvider.sendMail(email, 'Olá mundo ');
   }
 }
