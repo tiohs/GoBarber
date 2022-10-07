2d80af4ea5c8fdb4b2d2e43a1d372d6c7194494b
tiohs <hamiltonsilva551@gmail.com>
2022-10-07 23:55:02 +0100

    sessions Controller create

---
+++ Nivel 3/BACK/src/modules/users/infra/http/controllers/SessionsController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+
+export default class SessionsController {
+  public async create(request: Request, response: Response) {
+    const { email, password } = request.body;
+    const authenticateUser = container.resolve(AuthenticateUserService);
+
+    const { user, token } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    response.json({ user, token });
+  }
+}
