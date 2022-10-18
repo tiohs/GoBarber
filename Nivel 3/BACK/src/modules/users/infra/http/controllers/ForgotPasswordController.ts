1f39755e84c32782c9c8f7535ecb968e5643912d
tiohs <hamiltonsilva551@gmail.com>
2022-10-18 16:41:57 +0100

    feat: :zap: Send Forgot Password Email Service
    Send Forgot Password import to controllers

---
+++ Nivel 3/BACK/src/modules/users/infra/http/controllers/ForgotPasswordController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import SendForgotPasswordEmailService from '@modules/users/services/SendForgotPasswordEmailService';
+
+export default class SessionsController {
+  public async create(request: Request, response: Response) {
+    const { email } = request.body;
+    const sendForgotPasswordEmail = container.resolve(
+      SendForgotPasswordEmailService,
+    );
+
+    await sendForgotPasswordEmail.execute({
+      email,
+    });
+
+    response.status(204).json();
+  }
+}
