18ff75e630f56b8a6c79a7ec68f1b38e8dd7f214
tiohs <hamiltonsilva551@gmail.com>
2022-10-18 16:40:00 +0100

    Fix: Forgot Password

--- Nivel 3/BACK/src/modules/users/infra/http/routes/password.routes.ts
+++ Nivel 3/BACK/src/modules/users/infra/http/routes/password.routes.ts
@@ -1,19 +1,19 @@
-// import { Router } from 'express';
-// import { container } from 'tsyringe';
-// import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import { Router } from 'express';
+import { container } from 'tsyringe';
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
-// const sessionsRouter = Router();
+const sessionsRouter = Router();
 
-// sessionsRouter.post('/', async (request, response) => {
-//   const { email, password } = request.body;
-//   const authenticateUser = container.resolve(AuthenticateUserService);
+sessionsRouter.post('/', async (request, response) => {
+  const { email, password } = request.body;
+  const authenticateUser = container.resolve(AuthenticateUserService);
 
-//   const { user, token } = await authenticateUser.execute({
-//     email,
-//     password,
-//   });
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
 
-//   response.json({ user, token });
-// });
+  response.json({ user, token });
+});
 
-// export default sessionsRouter;
+export default sessionsRouter;
