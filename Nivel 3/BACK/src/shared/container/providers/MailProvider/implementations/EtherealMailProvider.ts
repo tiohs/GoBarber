ac94c508e648cb9ba2035fdab61400cb06b2c2a9
tiohs <hamiltonsilva551@gmail.com>
2022-10-19 20:58:54 +0100

    chore: :art: add in extends eslint

--- Nivel 3/BACK/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ Nivel 3/BACK/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,7 +1,24 @@
+import nodemailer, { Transporter } from 'nodemailer';
 import IMailProvider from '../models/IMailProvider';
 
 class FakeMailProvider implements IMailProvider {
-  private messages: IMessage[] = [];
+  private client: Transporter;
+
+  constructor() {
+    nodemailer.createTestAccount().then(account => {
+      const transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+
+      this.client = transporter;
+    });
+  }
 
   public async sendMail(to: string, body: string): Promise<void> {}
 }
