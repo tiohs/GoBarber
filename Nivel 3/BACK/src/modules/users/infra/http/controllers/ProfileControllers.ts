b495a69fa4249d170cea434faf0f4fe08de58ac4
tiohs <hamiltonsilva551@gmail.com>
2022-11-14 13:27:11 +0100

    feat: :fire: update profile
    update profile
    
    cool

---
+++ Nivel 3/BACK/src/modules/users/infra/http/controllers/ProfileControllers.ts
@@ -0,0 +1,20 @@
+// import { Response, Request } from 'express';
+
+// import { container } from 'tsyringe';
+// import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+
+// export default class ProfileController {
+//   public async update(request: Request, response: Response) {
+//     const user_id = request.user.id;
+//     const { name, email, old_password, password } = request.body;
+//     const createUser = container.resolve(CreateUserService);
+
+//     await update.execute({
+//       name,
+//       email,
+//       password,
+//       old_password,
+//     });
+//     return response.json({ name, email });
+//   }
+// }
