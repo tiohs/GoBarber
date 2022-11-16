e956ad89fa7b90f0bef75826f83b46c1102e6415
tiohs <hamiltonsilva551@gmail.com>
2022-11-16 17:37:13 +0100

    feat: :fire: Create Controllers Providers
    Create Controllers Providers
    
    Create Controllers Providers

---
+++ Nivel 3/BACK/src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProvidersService from '@modules/appointments/services/ListProvidersService';
+
+export default class AppointmentController {
+  public async create(request: Request, response: Response) {
+    const userId = request.user.id;
+
+    const listProvidersService = container.resolve(ListProvidersService);
+
+    const providers = await listProvidersService.execute({
+      userId,
+    });
+
+    return response.json(providers);
+  }
+}
