9e625c04bfdb49a6b9880e3cc07142157d40e02c
tiohs <hamiltonsilva551@gmail.com>
2022-11-17 19:44:04 +0100

    test: :zap: finish test should be able to list the month availability from provider
    finish test should be able to list the month availability from provider
    
    finish test should be able to list the month availability from provider

--- Nivel 3/BACK/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ Nivel 3/BACK/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -13,9 +13,13 @@ describe('UpdateProfile', () => {
   });
 
   it('should be able to list the month availability from provider', async () => {
-     await fakeAppointmentsRepository.create({
+    await fakeAppointmentsRepository.create({
       provider_id: 'user',
       date: new Date(2022, 11, 20, 8, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2022, 11, 20, 9, 0, 0),
     });
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
@@ -23,7 +27,44 @@ describe('UpdateProfile', () => {
     });
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
+      date: new Date(2022, 11, 20, 11, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2022, 11, 20, 12, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2022, 11, 20, 13, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2022, 11, 20, 14, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2022, 11, 20, 15, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2022, 11, 20, 16, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2022, 11, 20, 17, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
       date: new Date(2022, 11, 21, 8, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2022, 11, 21, 9, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2022, 11, 21, 10, 0, 0),
     });
     const availability = await listProviderMonthAvailability.execute({
       provider_id: 'user',
@@ -32,10 +73,10 @@ describe('UpdateProfile', () => {
     });
     expect(availability).toEqual(
       expect.arrayContaining([
-        { day: 19, availability: true },
-        { day: 20, availability: false },
-        { day: 21, availability: false },
-        { day: 22, availability: true },
+        { day: 19, available: true },
+        { day: 20, available: false },
+        { day: 21, available: true },
+        { day: 22, available: true },
       ]),
     );
   });
--- Nivel 3/BACK/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ Nivel 3/BACK/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { getDate, getDaysInMonth } from 'date-fns';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -24,13 +25,30 @@ export default class ListProviderMonthAvailabilityService {
     year,
     month,
   }: IRequest): Promise<IResponse> {
-    const appointment =
+    const appointments =
       await this.appointmentsRepository.findAllInMonthFromProvider({
         provider_id,
         year,
         month,
       });
 
-    return [{ day: 1, available: false }];
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(
+        appointment => getDate(appointment.date) === day,
+      );
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
