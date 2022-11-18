6159e520bed3ee433f25bf1e3f812ed33f58e9c8
tiohs <hamiltonsilva551@gmail.com>
2022-11-18 16:49:25 +0100

    test: :zap: refactory test & create test for should not be able to create an appointments on a past date
    refactory test & create test for should not be able to create an appointments on a past date
    
    refactory test & create test for should not be able to create an appointments on a past date

--- Nivel 3/BACK/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ Nivel 3/BACK/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,12 +2,17 @@ import AppError from '@shared/error/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakesAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let createAppointmentService: CreateAppointmentService;
+
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', async () => {
-    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    const createAppointmentService = new CreateAppointmentService(
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    createAppointmentService = new CreateAppointmentService(
       fakeAppointmentsRepository,
     );
+  });
+  it('should be able to create a new appointment', async () => {
     const appointment = await createAppointmentService.execute({
       date: new Date(),
       provider_id: '1',
@@ -18,12 +23,7 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointment on the same time', async () => {
-    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    const createAppointmentService = new CreateAppointmentService(
-      fakeAppointmentsRepository,
-    );
     const appointmentDate = new Date(2020, 4, 10, 11);
-
     await createAppointmentService.execute({
       date: appointmentDate,
       provider_id: '1',
@@ -38,5 +38,18 @@ describe('CreateAppointment', () => {
         user_id: '1',
       }),
     ).rejects.toBeInstanceOf(AppError); // Espera que o erro seja uma instacia de AppError
+  });
+  it('should not be able to create an appointments on a past date', async () => {
+    jest
+      .spyOn(Date, 'now')
+      .mockImplementationOnce(() => new Date(2020, 4, 10, 12).getTime());
+
+    await expect(
+      createAppointmentService.execute({
+        date: new Date(2020, 4, 10, 11),
+        provider_id: '1',
+        user_id: '1',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
