676e12949ed680f864eb74381fc9e593c1349cec
tiohs <hamiltonsilva551@gmail.com>
2022-11-22 22:40:37 +0100

    feat: :fire:

---
+++ Nivel 3/BACK/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,35 @@
+import { injectable, inject } from 'tsyringe';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+  day: number;
+}
+
+@injectable()
+export default class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    year,
+    month,
+    day,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments =
+      await this.appointmentsRepository.findAllInDayFromProvider({
+        provider_id,
+        year,
+        month,
+        day,
+      });
+
+    return appointments;
+  }
+}
