b5848aea5929f60ac86eed188a63eb2632a11707
tiohs <hamiltonsilva551@gmail.com>
2022-11-30 08:38:44 +0100

    ok

--- Nivel 3/BACK/src/modules/users/infra/http/routes/password.routes.ts
+++ Nivel 3/BACK/src/modules/users/infra/http/routes/password.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 import ResetPasswordControllers from '../controllers/ResetPasswordControllers';
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
 
@@ -7,7 +8,25 @@ const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
 const resetPasswordControllers = new ResetPasswordControllers();
 
-passwordRouter.post('/forgot', forgotPasswordController.create);
-passwordRouter.post('/reset', resetPasswordControllers.create);
+passwordRouter.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create,
+);
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  resetPasswordControllers.create,
+);
 
 export default passwordRouter;
