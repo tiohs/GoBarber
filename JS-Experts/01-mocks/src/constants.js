5289ed47e2a9f997e0ed213ea0c58c2478629139
tiohs <hamiltonsilva551@gmail.com>
2022-12-17 09:38:51 +0100

    File error

---
+++ JS-Experts/01-mocks/src/constants.js
@@ -0,0 +1,8 @@
+const constants = {
+  error: {
+    FILE_LENGTH_ERROR_MESSAGE: "the content length is invalid",
+    FILE_FIELDS_ERROR_MESSAGE: "the provider properties are invalid",
+  },
+};
+
+module.exports = constants;
