c4796a83f2ce9f4e193c7133866bff855e0fb060
tiohs <hamiltonsilva551@gmail.com>
2022-12-29 09:25:12 +0100

    ok

--- Nivel 4/01-fundamentos-react/src/App.jsx
+++ Nivel 4/01-fundamentos-react/src/App.jsx
@@ -1,9 +1,14 @@
 import { Header } from "./components/Header";
+import { wrapper } from "./App.module.css";
 import "./global.css";
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header />
+      <div className={wrapper}>
+        <aside>Ola</aside>
+        <main>Oka</main>
+      </div>
     </div>
   );
 }
