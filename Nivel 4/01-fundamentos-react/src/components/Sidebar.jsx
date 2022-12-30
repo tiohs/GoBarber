c8804d9864892f338713490094b842cd78b1a829
tiohs <hamiltonsilva551@gmail.com>
2022-12-30 11:46:04 +0100

    position avatar

--- Nivel 4/01-fundamentos-react/src/components/Sidebar.jsx
+++ Nivel 4/01-fundamentos-react/src/components/Sidebar.jsx
@@ -1,11 +1,13 @@
 import styles from "./Sidebar.module.css";
 import img from "../assets/bg.jpg";
+import imgAvatar from "../assets/dc.jpeg";
 
 export function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <img className={styles.cover} src={img} />
       <div className={styles.profile}>
+        <img src={imgAvatar} className={styles.avatar} />
         <strong>Hamilton Silva</strong>
         <span>Dev</span>
       </div>
