84733e20444ca6677ef1c81190139f3a293f4931
tiohs <hamiltonsilva551@gmail.com>
2023-01-12 18:20:00 +0100

    create const post

--- Nivel 4/01-fundamentos-react/src/App.jsx
+++ Nivel 4/01-fundamentos-react/src/App.jsx
@@ -3,6 +3,43 @@ import { wrapper } from "./App.module.css";
 import { Post } from "./components/Post";
 import "./global.css";
 import { Sidebar } from "./components/Sidebar";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/tiohs.png",
+      name: "Hamilton Silva",
+      role: "CEO @Unituenda",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera " },
+      { type: "paragraph", content: "Como ser um bom programador " },
+      {
+        type: "paragraph",
+        content: "Para ser um bom programador devemos saber criar ",
+      },
+    ],
+    publishedAt: new Date("2023-01-11 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTE @ Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera " },
+      { type: "paragraph", content: "Como ser um bom programador " },
+      {
+        type: "paragraph",
+        content: "Para ser um bom programador devemos saber criar ",
+      },
+    ],
+    publishedAt: new Date("2023-01-10 20:00:00"),
+  },
+];
 function App() {
   return (
     <div>
